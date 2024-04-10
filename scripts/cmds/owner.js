5501638a9a045d8e585850b32b1b52fe9fda3f35
sifatwadud <104209884+sifatwadud@users.noreply.github.com>
2024-04-10 16:33:45 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -3,39 +3,25 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-config: {
-  name: "owner",
-  aurthor:"Tokodori",// Convert By Goatbot Tokodori 
-   role: 0,
-  shortDescription: " ",
-  longDescription: "𝐋𝐨𝐚𝐝𝐢𝐧𝐠 𝐎𝐰𝐧𝐞𝐫'𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧'",
-  category: "admin",
-  guide: "{pn}"
-},
+  config: {
+    name: "owner",
+    author: "Tokodori", // Corrected author's name
+    shortDescription: " ",
+    longDescription: "𝐋𝐨𝐚𝐝𝐢𝐧𝐠 𝐎𝐰𝐧𝐞𝐫'𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧'",
+    category: "admin",
+    guide: "{pn}"
+  },
 
   onStart: async function ({ api, event }) {
-  try {
-    const ownerInfo = {
-      name: 'ITACHI UCHIHA',
-      gender: 'MALE',
-      age: '18+',
-      facebookLink: 'https://www.facebook.com/profile.php?id=100071825551845',
-    };
-
-    const bold = 'https://drive.google.com/uc?export=download&id=1XId0CYgC1Qm4yVNlixF4aKRghLMyWztL'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
-
-    const tmpFolderPath = path.join(__dirname, 'tmp');
-
-    if (!fs.existsSync(tmpFolderPath)) {
-      fs.mkdirSync(tmpFolderPath);
-    }
-
-    const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
-    const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
-
-    fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
-
-    const response = `
+    try {
+      const ownerInfo = {
+        name: 'ZERODAR',
+        gender: 'MALE',
+        age: '18+',
+        facebookLink: 'facevook.com/zeroday.was.hacker',
+        instagram: 'user_died_8_8', // Added missing closing quote
+      };
+      const response = `
 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧:🧾
 Name: ${ownerInfo.name}
 Gender: ${ownerInfo.gender}
@@ -43,18 +29,16 @@ Age: ${ownerInfo.age}
 Facebook: ${ownerInfo.facebookLink}
 `;
 
+      await api.sendMessage({
+        body: response
+      }, event.threadID, event.messageID);
 
-    await api.sendMessage({
-      body: response,
-      attachment: fs.createReadStream(videoPath)
-    }, event.threadID, event.messageID);
-
-    if (event.body.toLowerCase().includes('ownerinfo')) {
-      api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
+      if (event.body.toLowerCase().includes('ownerinfo')) {
+        api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
+      }
+    } catch (error) {
+      console.error('Error in ownerinfo command:', error);
+      return api.sendMessage('An error occurred while processing the command.', event.threadID);
     }
-  } catch (error) {
-    console.error('Error in ownerinfo command:', error);
-    return api.sendMessage('An error occurred while processing the command.', event.threadID);
-  }
-},
+  },
 };
