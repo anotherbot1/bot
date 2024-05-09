0eed1cefe0535c7ad924d4bb9aaccbeaf1e41821
sifatwadud <104209884+sifatwadud@users.noreply.github.com>
2024-05-10 00:43:00 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,47 +1,53 @@
-const fs = require("fs");
-const jimp = require("jimp");
-const axios = require("axios");
-const request = require("request");
-const moment = require("moment-timezone");
-
-const config = {
-  name: "info",
-  version: "1.0",
-  author: "Itachi",
-  countDown: 5,
-  role: 0,
-  shortDescription: "Admin and Bot information",
-  longDescription: "Admin and Bot information",
-  category: "𝗜𝗡𝗙𝗢",
-  guide: {
-    en: "{p}{n}",
-  },
-};
+const fs = require('fs');
+const moment = require('moment-timezone');
 
 module.exports = {
-  config,
+  config: {
+    name: "info",
+    version: "1.0",
+    author: "ZERODAY",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin "
+    },
+    longDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin "
+    },
+    category: "𝗜𝗡𝗙𝗢",
+    guide: {
+      en: "{pn}"
+    },
+    envConfig: {}
+  },
 
-  onStart: async function({ api, event, threadID })  {
-    const time = process.uptime(),
-          hours = Math.floor(time / (60 * 60)),
-          minutes = Math.floor((time % (60 * 60)) / 60),
-          seconds = Math.floor(time % 60);
+  onStart: async function ({ message }) {
+    const botName = "ZERODAY BOT";
+    const botPrefix = "/";
+    const authorName = "ZERODAY";
+    const authorFB = "https://www.facebook.com/zeroday.was.hacker";
+    const authorInsta = "https://www.instagram.com/user_died_8_8/";
+    const status = "Who cares!!";
+    const now = moment().tz('Asia/Dhaka');
+    const date = now.format('MMMM Do YYYY');
+    const time = now.format('h:mm:ss A');
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
-    var juswa = moment.tz("Asia/Dhaka").format("ã€ŽD/MM/YYYYã€ ã€hh:mm:ssã€‘");
-
-    var callback = () => api.sendMessage({
-      body: `ADMIN AND BOT INFORMATION 
-
-BOT NAME : Z E R O D A Y BOT
-FACEBOOK : https://www.facebook.com/zeroday.was.hacker
-BOT PREFIX : ${config.prefix}
-BOT OWNER : ZERODAY
-TIME : ${juswa} 
-BOT IS RUNNING ${hours}:${minutes}:${seconds}.
-
-THANKS FOR USING Z E R O D A Y BOT`
+    message.reply({
+      body: `===「 Bot & Owner Info 」===\n❏Bot Name: ${botName}\n❏Bot Prefix: ${botPrefix}\n❏Name: ${authorName}\n❏Facebook: ${authorFB}\n❏Instagram: ${authorInsta}\n❏Status: ${status}\n❏Date: ${date}\n❏Time: ${time}\n❏Uptime: ${uptimeString}\n=====================`
     });
+  },
 
-    callback();
+  onChat: async function({ event, message, getLang }) {
+    if (event.body && event.body.toLowerCase() === "info") {
+      this.onStart({ message });
+    }
   }
 };
