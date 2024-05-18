9858bb86db63444ce797f6a0218b51bb0a0b120f
sifatwadud <104209884+sifatwadud@users.noreply.github.com>
2024-05-18 17:17:03 +0600

    Update mention.js

--- scripts/cmds/mention.js
+++ scripts/cmds/mention.js
@@ -1,45 +1,50 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-
 module.exports.config = {
-    name: "mention",
-    version: "1.0.0",
-    author: "ZERODAY",
-    hasPermssion: 2,
-    description: "Mention him if he doesn't listen",
-    commandCategory: "𝗙𝗨𝗡",
-    usages: "{pn} mention",
-    cooldowns: 10,
-    dependencies: {
-        "fs-extra": "",
-        "axios": ""
-    }
+		name: "mention",
+		version: "1.0.0",
+		author: "ZERODAY",
+		hasPermssion: 2,
+		description:"Mention him if he doesn't listen" ,
+		commandCategory: "𝗙𝗨𝗡",
+		usages: "{pn} mention",
+		cooldowns: 10,
+		dependencies: {
+				"fs-extra": "",
+				"axios": ""
+		}
 }
 
-module.exports.onStart = async function({ api, args, Users, event }) {
-    try {
-        const mention = Object.keys(event.mentions)[0];
-        if (!mention) {
-            return api.sendMessage("Need to tag 1 user whom you want to start mention", event.threadID);
-        }
-
-        const name = event.mentions[mention];
-        const arraytag = [{ id: mention, tag: name }];
-
-        const sendMentionMessage = (message, delay) => {
-            setTimeout(() => {
-                api.sendMessage({ body: message, mentions: arraytag }, event.threadID);
-            }, delay);
-        };
-
-        const intervals = [0, 3000, 5000, 7000, 9000, 11000, 13000, 15000, 17000, 19000, 20000, 21500, 24000, 26000, 28000, 30000, 32000, 35000, 37000, 40000, 42000, 46000, 48000, 51000, 54000, 57000, 59000];
-        intervals.forEach((interval, index) => {
-            const message = index === 0 ? "hi" : `hi ${name}`;
-            sendMentionMessage(message, interval);
-        });
-
-    } catch (error) {
-        console.error("An error occurred while sending mentions:", error);
-        api.sendMessage("An error occurred while sending mentions. Please try again later.", event.threadID);
-    }
-};
+module.exports.onStart = async function({ api, args, Users, event}) {
+		var mention = Object.keys(event.mentions)[0];
+		if(!mention) return api.sendMessage("Need to tag 1 user whom you want to start mention", event.threadID);
+ let name =  event.mentions[mention];
+		var arraytag = [];
+				arraytag.push({id: mention, tag: name});
+		var a = function (a) { api.sendMessage(a, event.threadID); }
+a("hi");
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 3000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 5000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 7000);
+setTimeout(() => {a({body: " hi" + " " + name, mentions: arraytag})}, 9000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 11000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 13000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 15000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 19000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 20000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 21500);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 24000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 26000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 28000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 30000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 32000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 35000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 37000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 40000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 42000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 46000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 48000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 51000);
+setTimeout(() => {a({body: " hi" + " " + name, mentions: arraytag})}, 54000);
+setTimeout(() => {a({body: "hi" + " " + name, mentions: arraytag})}, 57000);
+setTimeout(() => {a("hi")} , 59000);
+}
