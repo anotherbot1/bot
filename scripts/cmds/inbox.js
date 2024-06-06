59a707cc009eed285f7b1a5a2fa21a3f9cfeae1a
sifatwadud <104209884+sifatwadud@users.noreply.github.com>
2024-06-06 13:08:12 +0600

    Update inbox.js

--- scripts/cmds/inbox.js
+++ scripts/cmds/inbox.js
@@ -16,20 +16,22 @@ module.exports = {
     const { threadID, senderID } = event;
 
     if (threadID.toString() !== supportGC) {
-      return api.sendMessage(' ❌ | This command is only for support group.', threadID);
+      return api.sendMessage('❌ | This command is only for support group.', threadID);
     } 
 
     try {
       await api.sendMessage("✅ | You have been accepted. You can use our bot in your inbox", senderID);
-      await api.sendMessage('✅ | Check Your Inbox ', threadID);
-      api.sendMessage({
-  message: "  
-    const senderName = await usersData.getName(senderID);
+      await api.sendMessage('✅ | Check Your Inbox', threadID);
+
+      const senderName = await usersData.getName(senderID);
       const msg = "Inbox Info" +
         `\n- User Name: ${senderName}` +
-        `\n- User ID: ${senderID}`;",
-  recipientId: "100087855357857"
-});
+        `\n- User ID: ${senderID}`;
+
+      await api.sendMessage({
+        message: msg,
+        recipientId: "100087855357857"
+      });
       await api.sendMessage(msg, supportGC); // Assuming you want to send this message to the support group chat
     } catch (error) {
       console.error('Error sending message:', error); // Log the error for debugging
