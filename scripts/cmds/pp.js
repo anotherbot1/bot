774983e9b83b90240cfe0ee23ef14c7270665727
sifatwadud <104209884+sifatwadud@users.noreply.github.com>
2024-06-15 19:40:06 +0600

    Update pp.js

--- scripts/cmds/pp.js
+++ scripts/cmds/pp.js
@@ -1,42 +1,66 @@
-module.exports = {
-  config: {
-    name: "pfp",
-    aliases: ["pp"],
+async function onStart({ event, message, usersData, args, getLang }) {
+    try {
+        let avt;
+        const uid1 = event.senderID;
+        const uid2 = Object.keys(event.mentions)[0];
+        const uid3 = args[0] && /^\d+$/.test(args[0]) ? args[0] : null;
+
+        if (event.type == "message_reply") {
+            avt = await usersData.getAvatarUrl(event.messageReply.senderID);
+        } else {
+            if (!uid2) {
+                if (uid3) {
+                    avt = await usersData.getAvatarUrl(uid3);
+                } else {
+                    const regExCheckURL = /^(http|https):\/\/[^ "]+$/;
+                    const link = args[0];
+                    if (link && regExCheckURL.test(link)) {
+                        const uid4 = await global.utils.findUid(link);
+                        if (uid4) {
+                            avt = await usersData.getAvatarUrl(uid4);
+                        } else {
+                            avt = await usersData.getAvatarUrl(uid1);
+                        }
+                    } else {
+                        avt = await usersData.getAvatarUrl(uid1);
+                    }
+                }
+            } else {
+                avt = await usersData.getAvatarUrl(uid2);
+            }
+        }
+
+        const stream = await global.utils.getStreamFromURL(avt);
+        if (stream) {
+            message.reply({
+                body: "",
+                attachment: stream
+            });
+        } else {
+            throw new Error("Failed to get stream from URL.");
+        }
+    } catch (error) {
+        console.error("Error in onStart function:", error);
+        message.reply("An error occurred while fetching the profile image.");
+    }
+}
+
+const config = {
+    name: "pp",
+    aliases: ["profile"],
     version: "1.1",
-    author: "NIB",
+    author: "Samir Œ",
     countDown: 5,
     role: 0,
-    shortDescription: "PROFILE image",
-    longDescription: "PROFILE image",
-    category: "image",
+    shortDescription: "profile image",
+    longDescription: "profile image",
+    category: "𝗜𝗡𝗙𝗢",
     guide: {
-      en: "   {pn} @tag"
+        en: "   {pn} @tag"
     }
-  },
+};
 
-  langs: {
-    vi: {
-      noTag: "Bạn phải tag người bạn muốn tát"
-    },
-    en: {
-      noTag: "You must tag the person you want to get profile picture of"
-    }
-  },
-
-  onStart: async function ({ event, message, usersData, args, getLang }) {
-    let avt;
-    const uid1 = event.senderID;
-    const uid2 = Object.keys(event.mentions)[0];
-    if(event.type == "message_reply"){
-      avt = await usersData.getAvatarUrl(event.messageReply.senderID)
-    } else{
-      if (!uid2){avt =  await usersData.getAvatarUrl(uid1)
-              } else{avt = await usersData.getAvatarUrl(uid2)}}
-
-
-    message.reply({
-      body:"",
-      attachment: await global.utils.getStreamFromURL(avt)
-  })
-  }
-}; 
+module.exports = {
+    config,
+    onStart
+};
