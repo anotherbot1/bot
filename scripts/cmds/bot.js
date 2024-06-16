c76b538fcd731f303d79ddcfe746f8cfe35b40cc
sifatwadud <104209884+sifatwadud@users.noreply.github.com>
2024-06-16 15:00:28 +0600

    Update bot.js

--- scripts/cmds/bot.js
+++ scripts/cmds/bot.js
@@ -1,33 +1,44 @@
 module.exports = {
   config: {
-    name: "noprefix",
+    name: "bot",
     version: "1.0",
-    author: "ZERODAY",
+    author: "Nova",
     countDown: 5,
     role: 0,
     shortDescription: "no prefix",
     longDescription: "no prefix",
-    category: "𝗢𝗪𝗡𝗘𝗥",
+    category: "𝗙𝗨𝗡",
   },
-
-  onStart: async function() {},
-
-  onChat: async function({ event, message, getLang, api }) {
-    if (event.body) {
-      const word = event.body.toLowerCase();
-      switch (word) {
-        case "bot":
-          message.reply({
-            body: "∘₊✧─────────────────✧₊∘\n Hey Naughty!!",
-          });
-          break; // Add this break statement to close the case
-
-        // You can add more cases here if needed
-
-        default:
-          // Add default behavior if necessary
-          break;
+  onStart: async function({ message, usersData, event }) {
+    try {
+      let name = await usersData.getName(event.senderID);
+      let id = event.senderID;
+      message.reply({
+        body: ` ∘₊✧─────────────────✧₊∘ ${name} Hey naughty!`,
+        mentions: [
+          { tag: name, id: id },
+        ]
+      });
+    } catch (error) {
+      console.error('Error in onStart function:', error);
+      message.reply('An error occurred while processing your request.');
+    }
+  },
+  onChat: async function({ event, message, usersData }) {
+    try {
+      if (event.body && (event.body.toLowerCase() == "bot" || event.body.toLowerCase() == "Zeroday")) {
+        let name = await usersData.getName(event.senderID);
+        let id = event.senderID;
+        message.reply({
+          body: `∘₊✧─────────────────✧₊∘ ${name} Hey naughty!`,
+          mentions: [
+            { tag: name, id: id },
+          ]
+        });
       }
+    } catch (error) {
+      console.error('Error in onChat function:', error);
+      message.reply('An error occurred while processing your request.');
     }
   }
 };
